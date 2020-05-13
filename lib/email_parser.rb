aff09030806744ac87bf80d6af339430990890cc
Greem <orangenewyorkcreativeagency@gmail.com>
2020-05-13 22:01:12 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,27 +1,47 @@
-# Build a class EmailAddressParser that accepts a string of unformatted 
+# Build a class EmailAddressParser that accepts a string of unformatted
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 require 'pry'
+#
+# class EmailAddressParser
+#  attr_accessor :name, :csv_emails #create instances
+#
+#     def initialize(csv_emails) #initialize email
+#         @csv_emails = csv_emails
+#     end
+#
+#
+#     def parse
+#
+#         csv_emails.split.map do |each_address|  #.split put all the emails into one array
+#
+#             each_address.split(',')
+#
+#         end.flatten.uniq
+#         #.uniq delete the duplicated emails
+#
+#     end
+#
+#
+# end
+
+
+#5/13/2020 4:57pm
 
 class EmailAddressParser
- attr_accessor :name, :csv_emails #create instances
+  attr_reader :csv_emails
 
-    def initialize(csv_emails) #initialize email
-        @csv_emails = csv_emails
-    end
+  def initialize(csv_emails)
+    @csv_emails = csv_emails
+  end
 
-
-    def parse
-        
-        csv_emails.split.map do |each_address|  #.split put all the emails into one array
-          
-            each_address.split(',')
-        
-        end.flatten.uniq
-        #.uniq delete the duplicated emails
-    
-    end
+  def parse
+     @csv_emails.split.map do |each| #["g@gmail.com,", "thi@gmail.com"]
+     puts "each::: #{each}" #g@gmail.com, thi@gmail.com
+     each.split(',')        #["g@gmail.com"]["thi@gmail.com"]
+     end.flatten.uniq
+   end
 
 
 end
