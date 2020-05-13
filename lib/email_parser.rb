971bcaa4984810049c9d967e41ae620c57168731
Greem <orangenewyorkcreativeagency@gmail.com>
2020-05-13 22:11:36 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -37,11 +37,10 @@ class EmailAddressParser
   end
 
   def parse
-     @csv_emails.split.map do |each| #["g@gmail.com,", "thi@gmail.com"]
-     puts "each::: #{each}" #g@gmail.com, thi@gmail.com
-     each.split(',')        #["g@gmail.com"]["thi@gmail.com"]
+     puts @csv_emails.split #["g@gmail.com,", "thi@gmail.com"] put all the email in one array.
+     @csv_emails.split.map do |each| #iterate each element in the array.
+     puts "each::: #{each}" #g@gmail.com, thi@gmail.com #each email with one comma
+     each.split(',')        #["g@gmail.com"]["thi@gmail.com"] #eliminate comma and put each element in each array
      end.flatten.uniq
    end
-
-
 end
